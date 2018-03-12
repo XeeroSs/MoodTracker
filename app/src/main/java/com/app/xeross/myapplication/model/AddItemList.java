f11bf8f57787d71d1884f9ea0241a2e994bf0c2e
XeeroSs <qmasini@gmail.com>
2018-03-12 19:53:16 +0100

    test
    Signed-off-by: XeeroSs <qmasini@gmail.com>

--- app/src/main/java/com/app/xeross/myapplication/model/AddItemList.java
+++ app/src/main/java/com/app/xeross/myapplication/model/AddItemList.java
@@ -18,6 +18,6 @@ public class AddItemList extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
 
         // For our recurring task, we'll just display a message
-        Toast.makeText(context, "I'm running", Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, "testt", Toast.LENGTH_SHORT).show();
     }
 }
